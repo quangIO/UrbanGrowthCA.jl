76bae7400c12b7acfa7eb07100a3090bb8ea4d30
Quang Luong <quangio@outlook.com>
2019-07-23 02:59:38 +0800

    calibration

--- src/simulation.jl
+++ src/simulation.jl
@@ -14,12 +14,11 @@ const policies = load_images_with_pattern(r"^policy\.\d+\.png$")
 
 const simulation_dim = size(lands[1])
 
-
 module Util
   using ArrayFire
   function gen_count_kernel()::Vector{AFArray{Float32,2}}
     hosts = [ones(Float32, s, s) for s = 3:2:7]
-    for i = 1:length(hosts)
+    @inbounds for i = 1:length(hosts)
         s = size(hosts[i])[1] - 1
         hosts[i][2:s, 2:s] .= 0
     end
@@ -36,29 +35,31 @@ struct ParameterConfig
   road_breed_factor::Vector{Float32}
 end
 
-function calculate_spread_probability(size::Number)::Float32 # TODO
+@fastmath function calculate_spread_probability(size::Number)::Float32 # TODO
 end
 
-urbanize(prob::Union{Real,AFArray}, idx::Number)::AFArray =
+@fastmath @inbounds urbanize(prob::Union{Real,AFArray}, idx::Number)::AFArray =
   rand(AFArray, simulation_dim) < prob .* policies[idx]
 
-count_neighbour(state::AFArray, idx::Number = 0x1)::AFArray =
+@inbounds @inbounds count_neighbour(state::AFArray, idx::Number = 0x1)::AFArray =
   convolve2(state, Util.neighbour_counts[idx], AF_CONV_DEFAULT, AF_CONV_AUTO)
 
 const config = ParameterConfig(0.00, [0.00], [0.1], 12, [0.00])
 
-function simulation(config::ParameterConfig, idx::Number)
+function simulate(config::ParameterConfig, idx::Number)
   breed_masks = [fill(1 - factor, simulation_dim) |> AFArray{Float32} for factor ∈ config.breed_factor]
   road_breed_masks = [fill(1 - factor, simulation_dim) |> AFArray{Float32} for factor ∈ config.road_breed_factor]
 
-  land = lands[idx]
-  road = roads[idx]
-  @showprogress for step = 1:100
+  @inbounds land = lands[idx]
+  @inbounds road = roads[idx]
+  for step = 1:20
+    
     # Diffusion
     diffusion = urbanize(config.diffusion_factor, idx)
     land |= diffusion
+    
     # Breed
-    for i = 1:length(config.breed_factor)
+    @inbounds for i = 1:length(config.breed_factor)
       neighbour = count_neighbour(land |> AFArray{UInt32}, i)
       breed = urbanize(1 - breed_masks[i]^neighbour, idx)
       land |= breed
@@ -78,8 +79,9 @@ function simulation(config::ParameterConfig, idx::Number)
     edge = maxfilt(region > 0, 3, 3, AF_PAD_ZERO)
     spread = urbanize(edge .* spread_probability, idx)
     land |= spread
+    
     # Road Breed
-    for i = 1:length(config.road_breed_factor)
+    @inbounds for i = 1:length(config.road_breed_factor)
       neighbour = count_neighbour(road, i)
       breed = urbanize(1 - road_breed_masks[i]^neighbour, idx)
       land |= breed
@@ -88,6 +90,21 @@ function simulation(config::ParameterConfig, idx::Number)
   return land
 end
 
-@time ret = simulation(ParameterConfig(0.00, [0], [1], 12, [0.00]), 1)
+@inbounds function evaluate(config::ParameterConfig, idx::Number)
+  predicted = resize(simulate(config, idx), 250, 250, AF_INTERP_NEAREST)
+  actual = resize(lands[idx + 1], 250, 250, AF_INTERP_NEAREST)
+  count_all((predicted & actual) + 0x0)[1] / count_all((predicted | actual) + 0x0)[1]
+end
 
+@fastmath @inbounds function evaluate(x::Vector)
+  config = ParameterConfig(x[1], [x[2]], [x[3]], 1000, [x[4]])
+  1 - evaluate(config, 1)
+end
+
+@time ret = simulate(ParameterConfig(0.00, [0.2], [1], 12, [0.00]), 1)
+ArrayFire.image(ret)
 save_image("test.png", ret |> AFArray{Float32})
+
+evaluate(ParameterConfig(0.00, [0.0], [0], 12, [0.00]), 1)
+
+res = bboptimize(evaluate; SearchRange = [(.001, .1), (.001, .1), (.001, .1), (.001, .1)], MaxTime=100.0)
